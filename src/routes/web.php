d8a155955bbb7886eb56782e5551e96c419d85c8
andrejfilkinq <andrejfilkinq@gmail.com>
2024-05-09 08:38:13 +0300

    modified .gitignore

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
 use App\Models\Task;
-use App\Models\Comment;
-use App\Models\Project;
+
 
 
 Route::get('/', function () {
-    $project = Project::find(1);
 
-    foreach ($project->tasks as $task) {
-        echo $task . '<br>';
-    }
 });
