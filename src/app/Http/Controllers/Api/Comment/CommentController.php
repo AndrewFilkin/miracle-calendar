d30a77a1fe385d9587567df98df6810a23d93f4c
andrejfilkinq <andrejfilkinq@gmail.com>
2024-05-13 20:58:16 +0300

    add CreateCommentService

--- src/app/Http/Controllers/Api/Comment/CommentController.php
+++ src/app/Http/Controllers/Api/Comment/CommentController.php
@@ -4,50 +4,14 @@ namespace App\Http\Controllers\Api\Comment;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Comment\CreateCommentRequest;
-use App\Models\Comment;
-use App\Models\File;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use App\Services\Api\Comment\CreateCommentService;
 
 class CommentController extends Controller
 {
 
-    public function create(CreateCommentRequest $request)
+    public function create(CreateCommentRequest $request, CreateCommentService $commentService)
     {
-        $creator = auth()->user()->id;
-
-        try {
-
-            $comment = Comment::create([
-                'user_id' => $creator,
-                'task_id' => $request->task_id,
-                'comment' => $request->comment,
-            ]);
-
-            if ($request->hasFile('files')) {
-                foreach ($request->file('files') as $file) {
-                    $fileOriginalName = $file->getClientOriginalName();
-                    $fileNameInStorage = Str::random(32) . '.' . $file->getClientOriginalExtension();
-                    $file->storeAs("public/files/task/$request->task_id/", $fileNameInStorage);
-
-                    //save data to db, table - files
-                    $file = new File([
-                        'user_id' => $creator,
-                        'file_name_in_storage' => $fileNameInStorage,
-                        'original_name' => $fileOriginalName,
-                    ]);
-                    $comment->files()->save($file);
-                }
-            }
-
-            if ($comment) {
-                return response()->json(['message' => 'Comment created'], 201);
-            } else {
-                return response()->json(['message' => 'Error comment created'], 500);
-            }
-        } catch (\Error $e) {
-            return response()->json(['message' => $e->getMessage()], 500);
-        }
+        $commentService->createComment($request);
+        return $commentService->answer;
     }
 }
---
+++ src/app/Services/Api/Comment/CreateCommentService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services\Api\Comment;
+
+use App\Http\Requests\Api\Comment\CreateCommentRequest;
+use App\Models\Comment;
+use App\Models\File;
+use Illuminate\Support\Str;
+
+class CreateCommentService
+{
+    public $answer;
+
+    public function createComment(CreateCommentRequest $request)
+    {
+        $creator = auth()->user()->id;
+
+        try {
+            $comment = Comment::create([
+                'user_id' => $creator,
+                'task_id' => $request->task_id,
+                'comment' => $request->comment,
+            ]);
+
+            if ($request->hasFile('files')) {
+                foreach ($request->file('files') as $file) {
+                    $fileOriginalName = $file->getClientOriginalName();
+                    $fileNameInStorage = Str::random(32) . '.' . $file->getClientOriginalExtension();
+                    $file->storeAs("public/files/task/$request->task_id/", $fileNameInStorage);
+
+                    //save data to db, table - files
+                    $file = new File([
+                        'user_id' => $creator,
+                        'file_name_in_storage' => $fileNameInStorage,
+                        'original_name' => $fileOriginalName,
+                    ]);
+                    $comment->files()->save($file);
+                }
+            }
+
+            if ($comment) {
+                $this->answer = response()->json(['message' => 'Comment created'], 201);
+            } else {
+                $this->answer = response()->json(['message' => 'Error comment created'], 500);
+            }
+        } catch (\Error $e) {
+            $this->answer = response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+}
