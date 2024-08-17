4855b0aff4ee5b663593cc596bc8c04a324f1d3f
AndrewFilkin <filkin.andrew.s@gmail.com>
2024-08-17 09:28:52 +0300

    Fix ShowAllTaskToAdminService

--- src/app/Services/Api/Task/ShowAllTaskToAdminService.php
+++ src/app/Services/Api/Task/ShowAllTaskToAdminService.php
@@ -254,8 +254,7 @@ class ShowAllTaskToAdminService
                         return $this->answer = response()->json($tasks);
                 }
             default:
-                $user = User::find($id);
-                $tasks = $user->tasks()->paginate(30);
+                $tasks = Task::paginate(30);
                 return $this->answer = response()->json($tasks);
         }
     }
