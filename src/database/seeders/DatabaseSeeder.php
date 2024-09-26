08679f90fdb7a04ab84a15c70d29c8e363ea7a31
AndrewFilkin <filkin.andrew.s@gmail.com>
2024-09-26 14:00:01 +0300

    change config for deploy

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\Checklist;
-use App\Models\Comment;
-use App\Models\File;
-use App\Models\Task;
-use App\Models\TaskUser;
+//use App\Models\Checklist;
+//use App\Models\Comment;
+//use App\Models\File;
+//use App\Models\Task;
+//use App\Models\TaskUser;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -23,18 +23,18 @@ class DatabaseSeeder extends Seeder
             'is_approved' => true,
         ]);
 
-        User::factory()->create([
-            'name' => 'Юзер Для Теста',
-            'email' => 'mcdermott.marielle@example.net',
-            'password' => Hash::make('11111111'),
-            'is_approved' => true,
-        ]);
+//        User::factory()->create([
+//            'name' => 'Юзер Для Теста',
+//            'email' => 'mcdermott.marielle@example.net',
+//            'password' => Hash::make('11111111'),
+//            'is_approved' => true,
+//        ]);
 
-        User::factory(200)->create();
-        Task::factory(400)->create();
-        TaskUser::factory(400)->create();
-        Comment::factory(1000)->create();
-        Checklist::factory(1000)->create();
-        File::factory(2000)->create();
+//        User::factory(200)->create();
+//        Task::factory(400)->create();
+//        TaskUser::factory(400)->create();
+//        Comment::factory(1000)->create();
+//        Checklist::factory(1000)->create();
+//        File::factory(2000)->create();
     }
 }
