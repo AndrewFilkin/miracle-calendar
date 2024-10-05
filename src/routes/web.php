acf40db9639e46ffebe08ef9d2594d7d88c2b34c
AndrewFilkin <filkin.andrew.s@gmail.com>
2024-10-05 14:17:38 +0300

    redirect to https://oblakozadach.ru

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Task;
+//use App\Models\Task;
 
 
 
-Route::get('/', function () {
-    phpinfo();
-});
+Route::get('{any}', function () {
+    return redirect()->to('https://oblakozadach.ru', 301);
+})->where('any', '^(?!api).*$');
 
-Route::get('show-concrete-task', function () {
-    return view('show-concrete-task');
-});
-
-Route::get('search', function () {
-    return view('search-user');
-});
-
-Route::get('send-vk-notification', function () {
-    return view('send-vk-notification');
-});
+//
+//Route::get('show-concrete-task', function () {
+//    return view('show-concrete-task');
+//});
+//
+//Route::get('search', function () {
+//    return view('search-user');
+//});
+//
+//Route::get('send-vk-notification', function () {
+//    return view('send-vk-notification');
+//});
